09ba04bab4431d19d6944967ce5cf74c13e2dd82
zhoaib <zhoaibhassan@gmail.com>
2022-10-25 15:59:03 +0600

    server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+
+app.get('/', (req, res) => {
+    res.send('course api');
+});
+
+app.get('/courses-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+    console.log('let-the-music-server running on port', port);
+})

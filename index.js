482b337cb4098f8a7ed7a4c81aeb7dfa4aa93633
zhoaib <zhoaibhassan@gmail.com>
2022-10-25 17:01:41 +0600

    new api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('course api');
@@ -13,8 +14,14 @@ app.get('/', (req, res) => {
 
 app.get('/courses-categories', (req, res) => {
     res.send(categories)
-})
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const seletedCourse = courses.find(course => course.id === id);
+
+});
 
 app.listen(port, () => {
     console.log('let-the-music-server running on port', port);
-})
+});
